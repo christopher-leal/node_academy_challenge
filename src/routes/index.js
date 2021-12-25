064db889104e5ba63ac1c195c584968e5731ae88
Christopher Leal <christopher.leal@globant.com>
2021-12-24 19:07:50 -0600

    articles routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import usersRouter from './users'
 import profilesRouter from './profiles'
+import articlesRouter from './articles'
 
 const router = Router()
 
 router.use('/', usersRouter)
 router.use('/profiles', profilesRouter)
+router.use('/articles', articlesRouter)
 
 export default router
