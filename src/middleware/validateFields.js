c00c8ee412d7384dde271fbb7d1b4f42b537040d
Christopher Leal <christopher.leal@globant.com>
2021-12-28 14:46:23 -0600

    custom error in validate fields middleware

--- src/middleware/validateFields.js
+++ src/middleware/validateFields.js
@@ -1,11 +1,18 @@
 import { validationResult } from 'express-validator'
 
 const validateFields = (req, res, next) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req).formatWith(errorFormatter)
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
+    return res.status(422).json({
+      errors: {
+        body: errors.array()
+      }
+    })
   }
   next()
+}
+const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+  return `${msg}`
 }
 
 export default validateFields
