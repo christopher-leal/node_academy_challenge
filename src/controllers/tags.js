863d3b11f5450b9a78898e8cea12860c06c644b3
Christopher Leal <christopher.leal@globant.com>
2021-12-28 17:59:35 -0600

    tags controller done

--- src/controllers/tags.js
+++ src/controllers/tags.js
@@ -1,6 +1,23 @@
-const getTags = async (req, res) => {
 
+import Tag from './../models/tag'
+import { sanitizeTagsName } from './../utils/sanitizeTagsName'
+import logger from './../utils/logger'
+
+const getTags = async (req, res) => {
+  try {
+    const tags = await Tag.findAll({})
+    return res.json({
+      success: true,
+      tags: sanitizeTagsName({ Tags: tags })
+    })
+  } catch (error) {
+    logger.error(error.message)
+    return res.status(422).json({
+      errors: { body: [error.message] }
+    })
+  }
 }
+
 export default {
   getTags
 }
