62ab2cd51630c0fcd86efdc7f6ac98c4a9c04171
Christopher Leal <christopher.leal@globant.com>
2021-12-29 15:14:14 -0600

    entry point created

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import logger from './utils/logger'
+import routes from './routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(helmet())
+
+app.use('/api', routes)
+
+app.get('/', (req, res) => {
+  logger.info('Server is online')
+  return res.json({
+    success: true,
+    message: 'Server online'
+  })
+})
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
 import dotenv from 'dotenv'
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import helmet from 'helmet'
 import logger from './utils/logger'
-import routes from './routes'
-import { connect } from './db/postgres'
+import './db/postgres'
 import './models'
+import app from './app'
 
 dotenv.config()
-
-const app = express()
-
-app.use(express.json())
-app.use(morgan('tiny'))
-app.use(cors())
-app.use(helmet())
-
-app.use('/api', routes)
-
-app.get('/', (req, res) => {
-  logger.info('Server is online')
-  return res.json({
-    success: true,
-    message: 'Server online'
-  })
-})
-
-connect()
 
 const PORT = process.env.PORT || 3000
 
