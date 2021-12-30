137cd31af6f19569ffb7400d1e6596e440f798db
Christopher Leal <christopher.leal@globant.com>
2021-12-29 20:12:52 -0600

    users routes tests added

---
+++ src/routes/users.test.js
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import app from './../app'
+
+describe('Testing user routes', () => {
+  it('POST /api/users should register an user', (done) => {
+    request(app)
+      .post('/api/users')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({ user: { email: 'chris@chris.com', password: 'password', username: 'chris' } })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        return done()
+      })
+  })
+
+  it('POST /api/users/login should not be authorized user', (done) => {
+    request(app)
+      .post('/api/users/login')
+      .send({ user: { email: 'chris@chris.com', password: 'bad-password' } })
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.errors.body[0]).toBe('email or password doesn\'t match')
+        return done()
+      })
+  })
+})
