33e4e04a7bbcc842473dcd934ecf144816bb47e4
Christopher Leal <christopher.leal@globant.com>
2021-12-30 17:45:47 -0600

    fix

--- src/db/redis.js
+++ src/db/redis.js
@@ -9,7 +9,7 @@ const url = baseUrl
   .replace('{host}', process.env.REDIS_HOST)
   .replace('{port}', process.env.REDIS_PORT)
 // // const url = 'rediss://:p4315cea4aff8616d9105c1646a8b245414750a4f68eca716ae8bcc6c01a96850@ec2-52-206-137-51.compute-1.amazonaws.com:28869'
-const client = createClient(url, { no_ready_check: true });
+const client = createClient('redis://default:password@localhost:6379', { no_ready_check: true });
 
 // const client = createClient('redis://:p4315cea4aff8616d9105c1646a8b245414750a4f68eca716ae8bcc6c01a96850@ec2-52-206-137-51.compute-1.amazonaws.com:28869')
 // console.log(url);
