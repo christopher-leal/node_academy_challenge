c6788c36de61f3a2a004ce55774e5029ac8770be
Christopher Leal <christopher.leal@globant.com>
2021-12-30 17:26:49 -0600

    fix

--- src/db/redis.js
+++ src/db/redis.js
@@ -3,12 +3,12 @@ import config from 'config'
 import logger from './../utils/logger'
 
 const baseUrl = config.get('redis.URL')
-const url = baseUrl
-  .replace('{user}', process.env.REDIS_USER)
-  .replace('{pass}', process.env.REDIS_PASSWORD)
-  .replace('{host}', process.env.REDIS_HOST)
-  .replace('{port}', process.env.REDIS_PORT)
-
+// const url = baseUrl
+//   .replace('{user}', process.env.REDIS_USER)
+//   .replace('{pass}', process.env.REDIS_PASSWORD)
+//   .replace('{host}', process.env.REDIS_HOST)
+//   .replace('{port}', process.env.REDIS_PORT)
+const url = 'redis://:p4315cea4aff8616d9105c1646a8b245414750a4f68eca716ae8bcc6c01a96850@ec2-52-206-137-51.compute-1.amazonaws.com:28869'
 const client = createClient('redis://:p4315cea4aff8616d9105c1646a8b245414750a4f68eca716ae8bcc6c01a96850@ec2-52-206-137-51.compute-1.amazonaws.com:28869')
 console.log(url);
 (async () => {
