51d48a8b6336d943462950b05fe909ff706eddd9
Christopher Leal <christopher.leal@globant.com>
2021-12-29 21:15:56 -0600

    dotenv added to script

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -2,7 +2,15 @@ import { Sequelize } from 'sequelize'
 import logger from './../utils/logger'
 import config from 'config'
 
-const sequelize = new Sequelize(config.get('postgres.DB_DATABASE'), config.get('postgres.DB_USER'), config.get('postgres.DB_PASSWORD'), {
+const baseUrl = config.get('postgres.URL')
+const url = baseUrl
+  .replace('{user}', process.env.DB_USER)
+  .replace('{pass}', process.env.DB_PASSWORD)
+  .replace('{host}', process.env.DB_HOST)
+  .replace('{port}', process.env.DB_PORT)
+  .replace('{db}', process.env.DB_DATABASE)
+
+const sequelize = new Sequelize(url, {
   host: config.get('postgres.DB_HOST'),
   port: config.get('postgres.DB_PORT'),
   dialect: 'postgres',
