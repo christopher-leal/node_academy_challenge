ea2e1f86f71734a27a2ddd230340e241c2a0aac7
Christopher Leal <christopher.leal@globant.com>
2021-12-30 18:47:44 -0600

    redis works in prod

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,19 +1,22 @@
 import { createClient } from 'redis'
 import config from 'config'
 import logger from './../utils/logger'
-
 const baseUrl = config.get('redis.URL')
+const { promisify } = require('util')
+
 const url = baseUrl
   .replace('{user}', process.env.REDIS_USER)
   .replace('{pass}', process.env.REDIS_PASSWORD)
   .replace('{host}', process.env.REDIS_HOST)
   .replace('{port}', process.env.REDIS_PORT)
 
-const client = createClient(url);
+const client = createClient(url)
+client.hGet = promisify(client.hget)
+client.hSet = client.hset
+client.hDel = client.hdel;
 
-(async () => {
+(() => {
   client.on('error', (err) => logger.error('Redis Client Error', err))
-  await client.connect()
   logger.info('Redis connection has been established successfully.')
 })()
 
