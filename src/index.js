73d0aac54a0008296b9f82f5911b618ef200c7d4
Christopher Leal <christopher.leal@globant.com>
2021-12-30 18:51:10 -0600

    connection removed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import logger from './utils/logger'
 import { connect } from './db/postgres'
 import './models'
 import app from './app'
-import { redisConnection } from './db/redis'
 
 connect()
-redisConnection()
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
